b891aedacdaeb86c0ddc81c9e507332edf600f81
Lydia <wenqix3@uw.edu>
2024-03-04 22:36:51 -0800

    Make the Dashboard Private

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/signin" />;
+}
